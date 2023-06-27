c7535108854132a29a7c8e39dc22d54933d06215
jardeveloper <jorgearios@protonmail.com>
2023-06-26 20:47:10 -0400

    Subida

---
+++ src/test/java/com/jarcoder/spring/boot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarcoder.spring.boot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
